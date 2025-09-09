d8dd885a9f507473a06d991366372b522309ed9b
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2025-09-09 18:28:43 -0300

    ajustes

--- aulas-react/src/exercises/exercise03/counterStore.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useCounter = create((set) => ({
-  count: 0,
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-  increment: () => set((state) => ({ count: state.count + 1})),
-}));
-
-export default useCounter;
--- aulas-react/src/exercises/exercise03/store.jsx
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-const useStore = create((set) => ({
-    state: 'started',
-    action: () => set({state: 'current'}),
-}));
